8ff24ec297ed37181bc64c5454e3a5502ad75b7d
praset <prasetyaarfanto@gmail.com>
2021-07-07 13:58:08 +0700

    Add PlaylistView

---
+++ iosApp/iosApp/Playlist/PlaylistView.swift
@@ -0,0 +1,21 @@
+//
+//  PlaylistView.swift
+//  iosApp
+//
+//  Created by Pras Adi on 07/07/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistView()
+    }
+}
