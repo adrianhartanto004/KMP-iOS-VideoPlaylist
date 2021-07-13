9690d0df24cddd2065bfb97b142eb71ae7a712f7
Adrian Hartanto <awi_dan@yahoo.com>
2021-07-13 21:53:33 +0700

    Add handling when network is error on register, login

--- shared/src/commonMain/kotlin/com/quipper/kmmplaylistexercise/shared/data/repository/LoginRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/quipper/kmmplaylistexercise/shared/data/repository/LoginRepositoryImpl.kt
@@ -5,18 +5,26 @@ import com.quipper.kmmplaylistexercise.shared.data.network.api.ExerciseApi
 import com.quipper.kmmplaylistexercise.shared.data.preferences.KmmPreferences
 import com.quipper.kmmplaylistexercise.shared.domain.model.LoginDomain
 import com.quipper.kmmplaylistexercise.shared.domain.repository.LoginRepository
+import io.ktor.utils.io.errors.*
 
 class LoginRepositoryImpl(
     private val kmmPreferences: KmmPreferences,
     private val exerciseApi: ExerciseApi
 ) : LoginRepository {
     override suspend fun postLogin(email: String, password: String): LoginDomain {
-        val response = exerciseApi.postLogin(email, password)
-        return if (response.token.isNotEmpty()) {
-            kmmPreferences.setString(Constants.LOGIN_TOKEN_KEY, response.token)
-            LoginDomain(response.token, "")
-        } else {
-            LoginDomain("", response.error)
+        try {
+            val response = exerciseApi.postLogin(email, password)
+            return if (response.token.isNotEmpty()) {
+                kmmPreferences.setString(Constants.LOGIN_TOKEN_KEY, response.token)
+                LoginDomain(response.token, "")
+            } else {
+                LoginDomain("", response.error)
+            }
+        } catch (throwable: Throwable) {
+            if (throwable is IOException) {
+                return LoginDomain("", "Network error\nPlease check your connection")
+            }
         }
+        return LoginDomain()
     }
 }
