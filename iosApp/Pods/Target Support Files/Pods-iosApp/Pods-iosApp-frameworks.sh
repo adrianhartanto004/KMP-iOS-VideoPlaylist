9228074d6f6a64ec074e57d554eb6e11df0a5557
Adrian Hartanto <awi_dan@yahoo.com>
2021-07-20 01:50:57 +0700

    Add auto-generated pod dependencies files

--- iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-frameworks.sh
+++ iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-frameworks.sh
@@ -175,11 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KMPNativeCoroutinesCombine/KMPNativeCoroutinesCombine.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KMPNativeCoroutinesCore/KMPNativeCoroutinesCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/sqlite3/sqlite3.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KMPNativeCoroutinesCombine/KMPNativeCoroutinesCombine.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KMPNativeCoroutinesCore/KMPNativeCoroutinesCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/sqlite3/sqlite3.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
